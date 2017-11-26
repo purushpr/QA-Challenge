cb7523e398932b4ea58d1afc5d3eeea98af67fb6
purushpr <prasath.1502@gmail.com>
2017-11-26 20:50:04 +0100

    Update Automation Script1.java

--- Automation Script1.java
+++ Automation Script1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class QAChallenge {
+public class TestCase1 {
 
 	public static void main(String[] args) throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\CHROME DRIVER\\chromedriver.exe");
